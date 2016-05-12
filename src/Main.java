8b524d6bc12900548f0e1e444d28dc0a64022c58
Nathan Oesterle <noesterle1@gmail.com>
2016-05-12 15:20:39 -0400

    The read contents are in an arraylist of arrays. Outter arraylist is like the file, inner is a row in DB.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,37 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by nathan on 5/10/16.
  */
 public class Main {
     public static void main(String[] args) {
         String file = "test.txt";
+        String arr[];
+        ArrayList <String[]> sql = new ArrayList();
+        String delim = "[~|]+";
+        String temp[];
         ReadFile read = new ReadFile(file);
-        //WriteSQL write = new WriteSQL();
+        WriteSQL write;
         String contents = read.readFile();
-        //System.out.println(contents);
-        //System.out.println(write.write());
+        arr = contents.split(delim);
+        System.out.println(arr[0]);
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            temp = arr[i].split(",");
+            sql.add(temp);
+        }
+        //Debugging print Statement
+        /*for (String tempname[] :sql) {
+            System.out.print("[");
+            for (String str: tempname
+                 ) {
+                System.out.print(str);
+                System.out.print(",");
+            }
+            System.out.print("]");
+        }
+        System.out.print("]");*/
+        write = new WriteSQL(sql);
     }
 }
--- src/WriteSQL.java
+++ src/WriteSQL.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+
 /**
  * Created by nathan on 5/10/16.
  */
 public class WriteSQL {
+    ArrayList <String[]> contents;
+    public WriteSQL(ArrayList <String[]> arrayList){
+        contents = arrayList;
+    }
+
     public Boolean write(){
         return true;
     }
