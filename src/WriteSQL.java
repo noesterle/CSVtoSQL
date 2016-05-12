55ec51fa750dfb80d454d722cea4e25f205f7401
Nathan Oesterle <noesterle1@gmail.com>
2016-05-12 18:05:08 -0400

    Sets up line to create table in text file.

--- src/WriteSQL.java
+++ src/WriteSQL.java
@@ -20,7 +20,13 @@ public class WriteSQL {
             FileOutputStream outputStream = new FileOutputStream(file);
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
             Writer w = new BufferedWriter(outputStreamWriter);
-            w.write("hi");
+            String header = createTable(contents);
+            /*for (String arr[]:contents) {
+                for (String str: arr) {
+                    w.append(str+" ");
+                }
+            }*/
+            w.write(header);
             w.close();
         }
         catch (Exception err){
@@ -31,5 +37,16 @@ public class WriteSQL {
 
     public File createFile(String filename){
         return new File(filename);
+    }
+
+    public String createTable(ArrayList <String[]> contents){
+        String[] header = contents.get(0);
+        String template = "CREATE TABLE tabl(";
+        for (String str:header) {
+            template = template + str + " none, ";
+        }
+        template = template.substring(0,template.length()-2) + ");";
+        //System.out.println(template);
+        return template;
     }
 }
